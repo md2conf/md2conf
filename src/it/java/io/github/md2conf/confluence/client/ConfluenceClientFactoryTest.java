9ea0797c53c41633a44a7731fdcd3ea946fbd08e
qwazer <resheto@gmail.com>
2021-09-22 20:33:15 +0300

    fixup! introduce model package & refactor renames: publisher -> client add ConfluenceClientFactory

--- src/it/java/io/github/md2conf/confluence/client/ConfluenceClientFactoryTest.java
+++ src/it/java/io/github/md2conf/confluence/client/ConfluenceClientFactoryTest.java
@@ -2,6 +2,7 @@ package io.github.md2conf.confluence.client;
 
 import io.github.md2conf.confluence.client.http.RequestFailedException;
 import io.github.md2conf.model.ConfluenceContent;
+import io.github.md2conf.model.ConfluenceContent.ConfluencePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,15 @@ import java.util.UUID;
 
 class ConfluenceClientFactoryTest extends AbstractContainerBaseTest {
 
+    private static final ConfluenceContent emptyContent = new ConfluenceContent(new ConfluencePage());
+
     @Test
     void test_create_client_with_not_existing_space() {
         ConfluenceClientConfigurationProperties properties = aDefaultConfluenceClientConfigurationProperties()
                 .withSpaceKey("no-such-space")
                 .build();
         RequestFailedException requestFailedException =  Assertions.assertThrows(RequestFailedException.class, () -> {
-            ConfluenceClientFactory.confluenceClient(properties, new ConfluenceContent(), null );
+            ConfluenceClientFactory.confluenceClient(properties, emptyContent, null );
         });
         Assertions.assertTrue(requestFailedException.getMessage().contains("No space with key"));
     }
@@ -26,7 +29,7 @@ class ConfluenceClientFactoryTest extends AbstractContainerBaseTest {
                 .withParentPageTitle("page" + UUID.randomUUID())
                 .build();
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            ConfluenceClientFactory.confluenceClient(properties, new ConfluenceContent(), null );
+            ConfluenceClientFactory.confluenceClient(properties, emptyContent, null );
         });
 
         Assertions.assertTrue(exception.getMessage().contains("There is no page with title"));
@@ -36,7 +39,7 @@ class ConfluenceClientFactoryTest extends AbstractContainerBaseTest {
     void test_create_client_with_existing_space_and_parent_page() {
         ConfluenceClientConfigurationProperties properties = aDefaultConfluenceClientConfigurationProperties()
                 .build();
-        ConfluenceClient confluenceClient = ConfluenceClientFactory.confluenceClient(properties, new ConfluenceContent(), null );
+        ConfluenceClient confluenceClient = ConfluenceClientFactory.confluenceClient(properties, emptyContent, null );
         Assertions.assertNotNull(confluenceClient);
     }
 }
