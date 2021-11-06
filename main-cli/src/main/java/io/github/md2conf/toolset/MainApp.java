d7521d3eefe2b07b6eedaa78487e972c7a6d0449
qwazer <resheto@gmail.com>
2021-11-06 22:51:37 +0300

    polish

--- main-cli/src/main/java/io/github/md2conf/toolset/MainApp.java
+++ main-cli/src/main/java/io/github/md2conf/toolset/MainApp.java
@@ -11,7 +11,7 @@ import picocli.CommandLine;
                 ModelOverviewCommand.class,
                 CommandLine.HelpCommand.class},
         description = "Set of tools to work with 'confluence-content-model': publish, dump, convert.",
-        footer = "'confluence-content-model' is a representation of Confluence page trees and attachments on a local filesystem. See 'md2conf help md2conf' for detailed format."
+        footer = "'confluence-content-model' is a representation of Confluence page trees and attachments on a local filesystem. See 'md2conf help md2conf' for details."
 )
 public class MainApp {
     public static void main(String[] args) {
