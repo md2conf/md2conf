53a6c7b59f42b30b86ddcafcb92866ad3b197ad9
qwazer <resheto@gmail.com>
2021-11-13 20:27:22 +0300

    improve coverage

---
+++ main-cli/src/test/java/io/github/md2conf/toolset/PublishCommandTest.java
@@ -0,0 +1,32 @@
+package io.github.md2conf.toolset;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import picocli.CommandLine;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Path;
+
+class PublishCommandTest {
+
+    @TempDir
+    private Path emptyDir;
+
+    @Test
+    void invoke_no_params() {
+        MainApp mainApp = new MainApp();
+        CommandLine cmd = new CommandLine(mainApp);
+        StringWriter swOut = new StringWriter();
+        StringWriter swErr = new StringWriter();
+        cmd.setOut(new PrintWriter(swOut));
+        cmd.setErr(new PrintWriter(swErr));
+        int exitCode = cmd.execute("publish");
+        Assertions.assertThat(exitCode).isNotZero();
+        String errOut = swErr.toString();
+        Assertions.assertThat(swOut.toString()).isEmpty();
+        Assertions.assertThat(errOut).isNotEmpty();
+    }
+
+}
