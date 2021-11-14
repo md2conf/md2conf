21f2a764bfdc14ac7a237bcd6b1c8c3fb18e23dc
qwazer <resheto@gmail.com>
2021-11-14 17:34:28 +0300

    rename "md2conf help md2conf" to "md2conf help model"

--- main-cli/src/main/java/io/github/md2conf/toolset/MainApp.java
+++ main-cli/src/main/java/io/github/md2conf/toolset/MainApp.java
@@ -8,10 +8,10 @@ import picocli.CommandLine;
                 PublishCommand.class,
                 ConvertAndPublishCommand.class,
                 DumpCommand.class,
-                ModelOverviewCommand.class,
+                ModelHelpCommand.class,
                 CommandLine.HelpCommand.class},
         description = "Set of tools to work with 'confluence-content-model': publish, dump, convert.",
-        footer = "'confluence-content-model' is a representation of Confluence page trees and attachments on a local filesystem. See 'md2conf help md2conf' for details."
+        footer = "'confluence-content-model' is a representation of Confluence page trees and attachments on a local filesystem. See 'md2conf help model' for details."
 )
 public class MainApp {
 
