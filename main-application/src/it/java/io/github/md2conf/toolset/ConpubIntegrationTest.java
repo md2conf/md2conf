91c1425d6c3f9a19c7c3ae3e876a0d6c796ba2f6
qwazer <resheto@gmail.com>
2023-02-25 18:30:58 +0300

    fixup! add WikiTitleRemover

--- main-application/src/it/java/io/github/md2conf/toolset/ConpubIntegrationTest.java
+++ main-application/src/it/java/io/github/md2conf/toolset/ConpubIntegrationTest.java
@@ -1,7 +1,6 @@
 package io.github.md2conf.toolset;
 
 import org.apache.commons.lang3.ArrayUtils;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import picocli.CommandLine;
@@ -33,7 +32,7 @@ class ConpubIntegrationTest extends AbstractContainerTestBase{
         // check sample page
         String id = super.pageIdBy("Sample");
         assertThat(id).isNotNull();
-        assertThat(pageBodyStorageById(id)).contains("<h1>Sample</h1>");
+        assertThat(pageBodyStorageById(id)).doesNotContain("<h1>Sample</h1>"); //first header removed from content and becomes the title
         assertThat(pageBodyStorageById(id)).contains("<ac:image><ri:attachment ri:filename=\"sample.gif\" /></ac:image>");
         assertThat(pageAttachmentsTitles(id)).contains("attachment.txt", "sample.gif" ).hasSize(2);
 
