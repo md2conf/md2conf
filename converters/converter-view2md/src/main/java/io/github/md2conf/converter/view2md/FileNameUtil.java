b601ce86ddbdc0ec3fabb47249a7cc6126728f92
qwazer <resheto@gmail.com>
2024-03-25 14:13:04 +0300

    md2conf-212 File name too long when invoke ConvertCommand

--- converters/converter-view2md/src/main/java/io/github/md2conf/converter/view2md/FileNameUtil.java
+++ converters/converter-view2md/src/main/java/io/github/md2conf/converter/view2md/FileNameUtil.java
@@ -2,13 +2,16 @@ package io.github.md2conf.converter.view2md;
 
 import org.apache.commons.io.FilenameUtils;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class FileNameUtil {
     /*
      * list of characters not allowed in filenames
      */
     public static final char[] INVALID_CHARS = {'\\', '/', ':', '*', '"', '<', '>', '|', '\'', ';', '=', ','};
     private static final char SANITIZED_CHAR = '_';
-    private static final int MAX_FILE_NAME_LENGHT = 255;
+    private static final int MAX_FILE_NAME_LENGHT_IN_BYTES = 255;
 
     /**
      * Given an input, return a sanitized form of the input suitable for use as
@@ -30,10 +33,15 @@ public class FileNameUtil {
     private static String shortenIfNeed(String filename) {
         String ext = FilenameUtils.getExtension(filename);
         String name = FilenameUtils.getName(filename);
-        if (filename.length() < MAX_FILE_NAME_LENGHT) {
+        if (filename.getBytes(StandardCharsets.UTF_8).length < MAX_FILE_NAME_LENGHT_IN_BYTES) {
             return filename;
         } else {
-            return filename.substring(0, MAX_FILE_NAME_LENGHT - ext.length() - 1) + "." + ext;
+            int maxBytes = MAX_FILE_NAME_LENGHT_IN_BYTES - 1 - ext.length();
+            if (maxBytes % 2 == 1) {
+                maxBytes = maxBytes - 1;
+            }
+            byte[] bytes = Arrays.copyOfRange(name.getBytes(), 0, maxBytes);
+            return new String(bytes, StandardCharsets.UTF_8) + "." + ext;
         }
     }
 }
