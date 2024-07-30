8df53ccb70691ee128bd501c93e1ed8941f94cb3
qwazer <resheto@gmail.com>
2024-07-30 17:48:34 +0300

    fixup! introduce IndexCommand.java

--- main-application/src/test/java/io/github/md2conf/toolset/IndexCommandTest.java
+++ main-application/src/test/java/io/github/md2conf/toolset/IndexCommandTest.java
@@ -1,10 +1,14 @@
 package io.github.md2conf.toolset;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import io.github.md2conf.indexer.PagesStructurePrinter;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
 import java.io.StringWriter;
 
+import static io.github.md2conf.toolset.TestUtil.addAppender;
 import static io.github.md2conf.toolset.TestUtil.getCommandLine;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +25,28 @@ class IndexCommandTest {
         assertThat(swErr.toString()).isNotEmpty().doesNotContain("Exception")
                 .contains("Usage: md2conf index (-i=<inputDirectory>")
                 .contains("Missing required argument");
+    }
+
+    @Test
+    void when_invokeWithInputDirectory_then_pageStructurePrinted() {
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        addAppender(listAppender, PagesStructurePrinter.class);
+        addAppender(listAppender, IndexCommand.class);
+
+        StringWriter swOut = new StringWriter();
+        StringWriter swErr = new StringWriter();
+        CommandLine cmd = getCommandLine(swOut, swErr);
+        String inputDir = "src/test/resources/markdown_example";
+        int exitCode = cmd.execute("index", "-i", inputDir );
+        assertThat(exitCode).isZero();
+        assertThat(swErr.toString()).isEmpty();
+        assertThat(listAppender.list).hasSize(4);
+        assertThat(listAppender.list.get(0).getFormattedMessage())
+                .isEqualTo("Indexing path src/test/resources/markdown_example");
+        assertThat(listAppender.list.get(2).getFormattedMessage())
+                .isEqualTo("Page structure is:");
+
     }
 
 
--- main-application/src/test/java/io/github/md2conf/toolset/TestUtil.java
+++ main-application/src/test/java/io/github/md2conf/toolset/TestUtil.java
@@ -1,5 +1,9 @@
 package io.github.md2conf.toolset;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 import java.io.PrintWriter;
@@ -13,5 +17,12 @@ public class TestUtil {
         cmd.setOut(new PrintWriter(swOut));
         cmd.setErr(new PrintWriter(swErr));
         return cmd;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void addAppender(ListAppender<ILoggingEvent> listAppender, Class clazz) {
+        Logger logger = (Logger) LoggerFactory.getLogger(clazz);
+        listAppender.start();
+        logger.addAppender(listAppender);
     }
 }
